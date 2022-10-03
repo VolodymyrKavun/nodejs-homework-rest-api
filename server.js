2499869ebc764258fbeb365aad35139e4cf944b2
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-10-03 15:22:45 +0300

    add_DB_HOST

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+const DB_HOST =
+  "mongodb+srv://Volodymyr:e08PVulLZuMuUNd5@cluster0.a1uqqq8.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// Підключення до бази даних
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
+
+// e08PVulLZuMuUNd5 Код для логіну
+// Volodymyr (Login)
