ab56618377ad83377bc911cc18fe774f1f853e32
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-10-03 16:13:41 +0300

    move DB_HOST to config.js

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
 
-const DB_HOST =
-  "mongodb+srv://Volodymyr:e08PVulLZuMuUNd5@cluster0.a1uqqq8.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
+
+// const DB_HOST =
+//   "mongodb+srv://Volodymyr:e08PVulLZuMuUNd5@cluster0.a1uqqq8.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 // Підключення до бази даних
 mongoose
