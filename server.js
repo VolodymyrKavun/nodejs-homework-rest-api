c5cea6ccbd12db8c8ab9267d5d1c3e8f5838f308
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-10-03 16:41:06 +0300

    add .env

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
+
+dotenv.config();
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
